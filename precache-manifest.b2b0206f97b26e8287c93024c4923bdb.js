0dad1cc557a632f52389e1f68b9f8b1f003e5440
Artem1332 <artem-averin2001@mail.ru>
2020-05-13 13:35:57 +0500

    Updates

---
+++ precache-manifest.b2b0206f97b26e8287c93024c4923bdb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b37b4b07bd20a658d9c9865731e61ea8",
+    "url": "/Vmeste/index.html"
+  },
+  {
+    "revision": "b49ba8b8bdb0295deb69",
+    "url": "/Vmeste/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "0add887c250a10677f96",
+    "url": "/Vmeste/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "b49ba8b8bdb0295deb69",
+    "url": "/Vmeste/static/js/2.2e1019aa.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/Vmeste/static/js/2.2e1019aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0add887c250a10677f96",
+    "url": "/Vmeste/static/js/main.8f3d68c8.chunk.js"
+  },
+  {
+    "revision": "f11732990ad98fd932aa",
+    "url": "/Vmeste/static/js/runtime-main.9abf68f2.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/Vmeste/static/media/persik.4e1ec840.png"
+  }
+]);
